ddb2710197476f8053c94af736054c84c04b3634
Bruno <bemds1@aluno.ifnmg.edu.br>
2023-01-08 06:25:44 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.modules.css'
+
+export function Header(){
+    return(
+        <header className={styles.header}>
+        <strong>Ignite Feed</strong>
+        </header>
+    )
+}
+
+
+
+
+
